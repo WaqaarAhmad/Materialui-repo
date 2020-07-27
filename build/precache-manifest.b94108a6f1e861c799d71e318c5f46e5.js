dd71acb08f1b684f52d3ef8e48ffb5b56689e8c0
Waqar Ahmad <waqaar.ahmad@yahoo.com>
2020-07-27 23:14:38 +0500

    Build

---
+++ build/precache-manifest.b94108a6f1e861c799d71e318c5f46e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c994ce6e3b5700ba71cbb885b2fc16f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751d44d463f205686271",
+    "url": "/static/css/main.cb53d2f9.chunk.css"
+  },
+  {
+    "revision": "5cf4761208078a67f3ba",
+    "url": "/static/js/2.4bee9eb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4bee9eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751d44d463f205686271",
+    "url": "/static/js/main.d61ca32e.chunk.js"
+  },
+  {
+    "revision": "e9c987836f4015f310d2",
+    "url": "/static/js/runtime-main.fda2ac98.js"
+  }
+]);
